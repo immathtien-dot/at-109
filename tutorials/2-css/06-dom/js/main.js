fd10f7d02c2c974501ad5c82f7c69662623e63a4
immathtien-dot <immathtien@gmail.com>
2026-03-02 23:27:24 -0800

    tut8+9

---
+++ tutorials/2-css/06-dom/js/main.js
@@ -0,0 +1,41 @@
+//console.log('javascript!')
+
+
+const header = document.querySelector('#header');
+const changeHeaderButton = document.querySelector('#change-header-button');
+const changeThemeButton = document.querySelector('#change-theme-button');
+const img1 = document.querySelector('#img1');
+const img2 = document.querySelector('#img2');
+const img3 = document.querySelector('#img3');
+
+// change headr with button click
+changeHeaderButton.addEventListener('click', () => {
+    header.innerHTML = 'POW!!!';
+})
+
+//toogle color theme
+
+// create function for changing button text
+function changeButtonText() {
+    if (document.body.classList.contains('dark')) {
+        changeThemeButton.textContent = 'Switch to the Light Theme';
+    } else {
+        changeThemeButton.textContent = 'Switch to the Dark Theme';
+    }
+}
+//click event on button
+changeThemeButton.addEventListener('click', () => {
+    // add/remove dark class to body
+    document.body.classList.toggle('dark');
+    changeButtonText();
+})
+
+//toogle image visibility
+
+img1.addEventListener("click", () => {
+    img2.classList.remove("hidden");
+})
+
+img2.addEventListener("click", () => {
+    img3.classList.remove("hidden");
+})
